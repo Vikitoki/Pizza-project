b73622b2d99c9a857d92a8e313c9e0e32c5de0de
Vasily <vasa-svistun@mail.ru>
2021-03-02 15:09:23 +0300

    Создан скелет проекта

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./styles/style.scss";
 
-import App from "./App";
+import App from "./components/app/app";
 
 ReactDOM.render(
   <React.StrictMode>
